34f5b235c9e1f415f88c28b4e2d786a1888bf579
Nguyen Trong Nhat <nguyentrongnhat240700@gmail.com>
2023-01-01 21:21:19 +0700

    build phần tính năng, sản phẩm, danh mục, đánh giá, blogs

--- js/MY-CUSTOM.js
+++ js/MY-CUSTOM.js
@@ -1,28 +1,30 @@
 /*------------------------------------------------
     Hiển thị Box Nội dung khi nhấn icon - header
 -------------------------------------------------*/
-const btnList = document.querySelectorAll('.btn-header');
-const boxList = document.querySelectorAll('.box');
-const ArrID = ['navbar', 'search-form', 'shopping-cart', 'login-form'];
+const btnList = document.querySelectorAll('div.icons > div');
+const boxList = document.querySelectorAll('.box-header');
+const ArrBoxID = ['navbar', 'search-form', 'shopping-cart', 'login-form'];
 
 // 1. Dò tất cả các button có class giống nhau:
 for (let i = 0; i < btnList.length; i++) {
     btnList[i].addEventListener('click', () => {
-        // 2. Dò lại và xoá class 'btn-active' có sẵn:
-        for (let i = 0; i < btnList.length; i++) {
-            btnList[i].classList.remove('btn-active');
+        // 2. Dò lại và xoá class 'btn-active' có sẵn (trừ click nút đang có sẵn):
+        if (!btnList[i].classList.contains('active')) {
+            for (let i = 0; i < btnList.length; i++) {
+                btnList[i].classList.remove('active');
+            }
         }
 
-        // 3. thêm class 'btn-active' vừa chọn:
-        btnList[i].classList.toggle('btn-active');
+        // 3. Thêm class 'btn-active':
+        btnList[i].classList.toggle('active');
 
         // 4. hiển thị form nội dung theo button đã chọn:
         let btnID = btnList[i].getAttribute('id');
-        for (let i = 0; i < ArrID.length; i++) {
-            if (ArrID[i] == btnID) {
-                document.querySelector('.' + ArrID[i]).classList.toggle('active');
+        for (let i = 0; i < ArrBoxID.length; i++) {
+            if (ArrBoxID[i] == btnID) {
+                document.querySelector('.' + ArrBoxID[i]).classList.toggle('active');
             } else {
-                document.querySelector('.' + ArrID[i]).classList.remove('active');
+                document.querySelector('.' + ArrBoxID[i]).classList.remove('active');
             }
         }
     });
@@ -36,8 +38,8 @@ window.onscroll = () => {
         for (let i = 0; i < btnList.length; i++) {
             btnList[i].classList.remove('active');
         }
-        for (let i = 0; i < boxList.length; i++) {
-            ArrID.classList.remove('active');
+        for (let i = 0; i < ArrBoxID.length; i++) {
+            document.querySelector('.' + ArrBoxID[i]).classList.remove('active');
         }
     }, 200);
 };
@@ -45,7 +47,6 @@ window.onscroll = () => {
 // ==========================================
 function toggleShowPassword() {
     var x = document.getElementById('password');
-    // var x = document.getElementsByClassName('.password');
     if (x.type === 'password') {
         x.type = 'text';
     } else {
---
+++ js/swiper-slider-custom.js
@@ -0,0 +1,59 @@
+var swiper = new Swiper('.product-slider', {
+    loop: true,
+    spaceBetween: 30,
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
+    autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        // ĐIỆN THOẠI DỌC MAX 180px -> 479px
+        0: {
+            slidesPerView: 1,
+        },
+        // ĐIỆN THOẠI NGANG MAX 480px -> 767px
+        480: {
+            slidesPerView: 2,
+        },
+        // ĐIỆN THOẠI NGANG MAX 768px -> 1023
+        768: {
+            slidesPerView: 3,
+        },
+        1024: {
+            slidesPerView: 4,
+        },
+    },
+});
+
+var swiper = new Swiper('.review-slider', {
+    loop: true,
+    spaceBetween: 30,
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
+    autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        // ĐIỆN THOẠI DỌC MAX 180px -> 479px
+        0: {
+            slidesPerView: 1,
+        },
+        // ĐIỆN THOẠI NGANG MAX 480px -> 767px
+        480: {
+            slidesPerView: 2,
+        },
+        // ĐIỆN THOẠI NGANG MAX 768px -> 1023
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
